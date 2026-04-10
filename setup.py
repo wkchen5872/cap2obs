82c8abfe208538b7e5bcae4fa89b6f2b6de920ba
wenkai chen <wenkai@fio.one>
2026-04-10 22:25:21 +0800

    chore: bump package version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cap2obs",
-    version="0.1.2",
+    version="0.2.0",
     author="Cap2Obs Contributors",
     description="Smart synchronization tool for Capacities backups to Obsidian vaults",
     long_description=long_description,
